ea62c65d2a12ec87f751d71a4444e3ffb8903daa
Garret Heaton <powdahound@gmail.com>
2012-12-17 09:13:52 -0800

    Remove emoticon stuff. Not relevant here.

--- javascript/linkify.js
+++ javascript/linkify.js
@@ -5,21 +5,6 @@ linkify = {
   RE_URL_MIDCHAR: '[\\S]',  // characters allowed in URL center
   RE_URL_ENDCHAR: '[\\w@?^=%&/~\+#-]',              // URL must end with one of these chars
   RE_URL_SCHEME: '(?:http|ftp|https|news|mms)://',  // protocol
-    
-  // Text to turn in to emoticon images
-  // "8)" must have a non-word character before it to protect "(2007-2008)" case
-  emoticons: [
-    {regex: ':-?D',     shortcut: ':D',   width: 18,  height: 18,    image: 'bigsmile.png'},
-    {regex: ':-?o',     shortcut: ':o',   width: 18,  height: 18,    image: 'gasp.png'},
-    {regex: ':-?p',     shortcut: ':p',   width: 18,  height: 18,    image: 'tongue.png'},
-    {regex: '8-?\\)',   shortcut: '8)',   width: 18,  height: 18,    image: 'cool.png'},
-    {regex: ':-?\\(',   shortcut: ':(',   width: 18,  height: 18,    image: 'frown.png'},
-    {regex: ':-\\*',    shortcut: ':-*',  width: 18,  height: 18,    image: 'kiss.png'},
-    {regex: ':\\\\',    shortcut: ':\\',  width: 18,  height: 18,    image: 'slant.png'},
-    {regex: ':-?\\)',   shortcut: ':)',   width: 18,  height: 18,    image: 'smile.png'},
-    {regex: ':-?\\|',   shortcut: ':|',   width: 18,  height: 18,    image: 'straightface.png'},
-    {regex: ';-?\\)',   shortcut: ';)',   width: 18,  height: 18,    image: 'wink.png'}
-  ],
 
   // Intialize the linkification variables
   // Call this before calling linkify(...)
@@ -28,55 +13,26 @@ linkify = {
     this.RE_FULL_URL = this.RE_URL_SCHEME+'\\w+(?:.\\w+)'+this.RE_URL_ENDING;
     this.RE_OTHER_URL = '\\w[\\w_-]*(?:\\.\\w[\\w_-]*)*\\.'+this.RE_TLD+'(?:\\/'+this.RE_URL_ENDING+')?\\b';
   },
-  
-  /**
-   * Add an word-based emoticon to the list of emoticons to check for
-   *
-   * @param filename - Name of the image file (the full path is created in the emoticon_text function)
-   * @param shortcut - Text used to create the emoticon (e.g. "embarrassed" or "puking" )
-   * @param height - Height in pixels of the image
-   * @param width - Width in pixels of the image
-   **/
-  add_emoticon: function(filename, shortcut, height, width) {
-    var emoticon_data = {
-      image: filename,
-      shortcut: shortcut,
-      height: height,
-      width: width,
-      regex: '\\('+shortcut+'\\)',
-      shortcut: '('+shortcut+')'
-    };
-    this.emoticons.push(emoticon_data);
-  },
-    
+
   /**
    * Linkify a string, replacing text urls with <a href="url">url</a>
    * Note: init must be called before this function can be used
    *
    * @param text - String to be linkified
-   * @param emoticonify - Whether to replace text emoticons with images
    * @param matched_links - Return param (pass by ref) - Array of links matched during linkification
    **/
-  linkify: function(text, emoticonify, truncate_length, matched_links) {
-    if (typeof emoticonify == 'undefined') {
-      emoticonify = true;
-    }
-    
+  linkify: function(text, truncate_length, matched_links) {
     if (typeof matched_links == 'undefined') {
       match_and_replace = [];
     }
-    
+
     text = this.match_and_replace(this.RE_EMAIL_PATTERN, text, false, false, truncate_length, matched_links);
     text = this.match_and_replace(this.RE_FULL_URL, text, true, false, truncate_length, matched_links);
     text = this.match_and_replace(this.RE_OTHER_URL, text, true, true, truncate_length, matched_links);
-    
-    if (emoticonify) {
-      text = this.emoticon_text(text);
-    }
-      
+
     return text;
   },
-  
+
   /**
    * Internal helper function for linkification
    **/
@@ -84,14 +40,14 @@ linkify = {
     if (typeof add_http == 'undefined') {
       add_http = true;
     }
-    
+
     var start = 0;
     var offset = 0;
     var match_length = 0;
     var end_tag_pos = 0;
     var close_anchor_re = /<\/[aA]>/;
     var re = new RegExp(pattern, "g");
-    
+
     var match = {};
     var max_iter = 20;
     var cur_iter = 0;
@@ -99,7 +55,7 @@ linkify = {
       cur_iter++;
       if (cur_iter > max_iter)
         break;
-      
+
       start = match.index; // start of match
       match_length = match[0].length;
 
@@ -108,17 +64,17 @@ linkify = {
       if (substr.search(/<[aA]/) >= 0) {
         close_anchor_re.lastIndex = offset;
         var close_anchor_pos = input.substring(offset, input.length).search(close_anchor_re);
-        
+
         // If we find an opening tag without a matching closing tag, just return the input we have
         if (close_anchor_pos < 0)
           return input;
-          
+
         end_tag_pos = close_anchor_pos + offset;
 
         // RegExp.lastIndex is used to tell the regexp where to start matching
         re.lastIndex = end_tag_pos + 4;
         offset = end_tag_pos + 4;
-        
+
         continue;
       }
 
@@ -130,7 +86,7 @@ linkify = {
         actual = 'http://'+actual;
       }
       var replacement = '<a target="_blank" href="';
-      
+
       if (!is_url) {
         replacement += 'mailto:';
       }
@@ -138,43 +94,28 @@ linkify = {
       actual = actual.replace(/"/g, '%22');
       replacement += actual+'"';
       replacement += ' title="';
-      
+
       if (is_url) replacement += actual;
       else replacement += 'Email '+actual;
-      
+
       replacement += '"';
-      
+
       if (truncate_length && address.length > truncate_length) {
             address = address.substr(0, truncate_length) + '...';
         }
-      
+
       // Add word break tags to allow wrapping where appropriate
       replacement += '>'+address.replace(new RegExp("([/=])", 'g'), "<wbr>$1")+'</a>';
       if (matched_links) {
         matched_links.push(actual);
       }
-      
+
       input = input.slice(0, start) + replacement + input.slice(start+match_length, input.length);
       re.lastIndex = start + replacement.length;
       offset = start + replacement.length;
     }
-    
+
     return input;
-  },
-  
-  /**
-   * Replace text emoticons with images
-   **/
-  emoticon_text: function(text) {
-    for (var idx in this.emoticons) {
-      var e_data = this.emoticons[idx];
-      var regex = e_data.regex;
-      var img = e_data.image;
-      
-      // (not a word character)(smiley regex)(not a word character)
-      var pattern = new RegExp('(^|\\W)'+regex+'(?=\\W|$)', 'gim');
-      text = text.replace(pattern, '$1<img name="emoticon" alt="'+e_data.shortcut+'" height="'+e_data.height+'" width="'+e_data.width+'" src="/path/to/emoticon/'+img+'" />');
-    }
-    return text;
   }
+
 };
